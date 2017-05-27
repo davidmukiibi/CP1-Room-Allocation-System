425bc6e6359ea3fa158e1bc4e7d7deeca71751c2
davidmukiibi <david.mukiibiq@gmail.com>
2017-05-27 08:56:00 +0300

    #146198111] ft-add-create-room-thod, this ethod creates the rooms in amity.

--- classes/amity.py
+++ classes/amity.py
@@ -5,3 +5,26 @@ class Amity(object):
     def __init__(self):
         self.rooms = {'offices':{}, 'living_spaces':{}, 'unallocated':{'office': [],\
                                                                     'living_space': [],},}
+
+    def create_room(self, room_name, room_type):
+          """this method creates a room, either an office or a living space
+          and adds it to the respectives dictionary values"""
+
+          if isinstance(room_name, str) and isinstance(room_type, str):
+              if room_type.lower() == 'o':
+                  office = Office(room_name=room_name, room_type='o')
+                  self.rooms['offices'].update({office.room_name: office.office_occupants})
+                  print '{} office created successfully.'.format(office.room_name)
+
+              elif room_type.lower() == 'l':
+                  lspace = LivingSpace(room_name=room_name, room_type='l')
+                  self.rooms['living_spaces'].update({lspace.room_name: lspace.living_space_occupants
+                                                     })
+                  print '{} living space created successfully.'.format(lspace.room_name)
+
+              else:
+                  print '{} wrong room type entered.'.format(room_type)
+
+          else:
+              print 'Either {} or {} is not a string, please check and try again!'.format(room_name,
+                                                                                          room_type)
