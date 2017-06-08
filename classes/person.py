df604a6cb9002cd3a7b512765727f21dff2bba29
davidmukiibi <david.mukiibiq@gmail.com>
2017-06-08 20:18:34 +0300

    [Chore] Clean out the white spaces.

--- classes/person.py
+++ classes/person.py
@@ -1,8 +1,7 @@
 class Person(object):
     """Person class blueprint for the staff and fellow classes.
     It lays the foundation for the fellow and staff classes and the eventually for the
-    their objects.
-    """
+    their objects."""
     def __init__(self, first_name, second_name, person_type, lspace_option='N'):
         self.first_name = first_name
         self.second_name = second_name
@@ -12,30 +11,16 @@ class Person(object):
         self.allocated_office = None
         self.allocated_living_space = None
 
-
-
 class Staff(Person):
     """new staff blueprint but also inherits from the Person class"""
 
     def __init__(self, first_name, second_name, person_type='STAFF', lspace_option='N'):
         super(Staff, self).__init__(first_name, second_name, person_type,
                                     lspace_option='N')
-        # self.name1 = first_name
-        # self.name2 = second_name
-        # self.type = person_type
-        # self.option = lspace_option
-
 
 class Fellow(Person):
-    """New fellow blueprint but also inherits from the Person class
-    person type(f) translates to fellow and (s) to staff.
-    """
+    """New fellow blueprint but also inherits from the Person class."""
 
     def __init__(self, first_name, second_name, person_type='FELLOW', lspace_option='N'):
         super(Fellow, self).__init__(first_name, second_name, person_type,
                                      lspace_option='N')
-        # self.name1 = first_name
-        # self.name2 = second_name
-        # self.type = person_type
-        # self.option = lspace_option
-
